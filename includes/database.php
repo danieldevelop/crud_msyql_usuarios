998f44fe8b3ce022c3dbf42aa58f26822b44db5f
Daniel Gomez <dgomez.informatica@gmail.com>
2022-12-26 16:06:32 -0300

    conexion con la base de datos crudmysql

---
+++ includes/database.php
@@ -0,0 +1,15 @@
+<?php
+
+include './config.php';
+
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_BBDD);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+    exit();
+} 
+
+if (!mysqli_select_db($conn, DB_BBDD)) {
+    die("Error: " . mysqli_error($conn));
+    exit();
+}
