89b805dc7aacd825a5480e080838d8f06fca40e3
Daniel Gomez <dgomez.informatica@gmail.com>
2022-12-27 01:25:13 -0300

    refactor: se movio los datos al la pantalla principal, antes estaban en la funcion listarUsuarios

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
 
 require_once './includes/functions.php';
+$all_usuarios = listarUsuarios();
 ?>
 
 <!DOCTYPE html>
@@ -58,17 +59,47 @@ require_once './includes/functions.php';
                                     <td colspan="8" class="text-center">No existen registros</td>
                                 </tr>
 
-                                <?php } else { ?>
-                                    <?= listarUsuarios(); ?>
-                                    <td colspan="2" class="text-center">
-                                        <a href="upd-user.php?idcod=<?php echo $fila['idcod']; ?>" class='btn btn-sm btn-success' title='editar'>
-                                            <img src='./img/pencil-square.svg' alt='icon bootstrap' class='img-fluid'>
-                                        </a>
-                                        <a href="javascript:confirmDelete('del-user.php?idcod=<?php echo $fila["idcod"]; ?>')" class='btn btn-sm btn-danger' title='eliminar'>
-                                            <img src='./img/trash.svg' alt='icon bootstrap' class='img-fluid'>
-                                        </a>
-                                    </td>
-                                </tr>
+                                <?php } else { 
+                                    foreach($all_usuarios as $a_usuario): ?>
+                                        <tr>
+                                            <?php 
+                                                if ($a_usuario['nacionalidad'] == "-1"):
+                                                    $nacionalidad = '';
+                                                else:
+                                                    $nacionalidad = ucfirst($a_usuario['nacionalidad']);
+                                                endif;
+                                        
+                                                if ($a_usuario['sexo'] == "M"):
+                                                    $sexo = 'Masculino';
+                                                elseif ($a_usuario['sexo'] == "F"):
+                                                    $sexo ='Femenina';
+                                                elseif ($a_usuario['sexo'] == "P" || $a_usuario['sexo'] == "PND"):
+                                                    $sexo = 'Prefiero no decirlo';
+                                                elseif ($a_usuario['sexo'] == "IND"):
+                                                    $sexo = 'Indeterminado';
+                                                elseif (empty($a_usuario['sexo'])):
+                                                    $sexo = '';
+                                                endif;
+                                            ?>
+
+                                            <td><?= $a_usuario['idcod']; ?></td>
+                                            <td><?= $a_usuario['rut']; ?></td>
+                                            <td><?= $a_usuario['nombre']; ?></td>
+                                            <td><?= $nacionalidad; ?></td>
+                                            <td><?= $sexo; ?></td>
+                                            <td><?= $a_usuario['fchNacimiento']; ?></td>
+                                            <td><?= $a_usuario['username']; ?></td>
+
+                                            <td colspan="2" class="text-center">
+                                                <a href="upd-user.php?idcod=<?php echo $a_usuario['idcod']; ?>" class='btn btn-sm btn-success' title='editar'>
+                                                    <img src='./img/pencil-square.svg' alt='icon bootstrap' class='img-fluid'>
+                                                </a>
+                                                <a href="javascript:confirmDelete('del-user.php?idcod=<?php echo $a_usuario['idcod']; ?>')" class='btn btn-sm btn-danger' title='eliminar'>
+                                                    <img src='./img/trash.svg' alt='icon bootstrap' class='img-fluid'>
+                                                </a>
+                                            </td> 
+                                        </tr>
+                                    <?php endforeach; ?>
                                 <?php } ?>
                             </tbody>
                         </table>
